82c06a8b4ee3914822064b4578e52882836e43a6
Alla Yakymova <allochkaja112@gmail.com>
2025-01-16 23:50:24 +0100

    changelog settings

--- generate-changelog.js
+++ generate-changelog.js
@@ -2,63 +2,101 @@ const fs = require('fs');
 const gitLogParser = require('git-log-parser');
 const Handlebars = require('handlebars');
 const path = require('path');
+const { Stream } = require('stream');
 
-// Настройка шаблона
 const templateSource = fs.readFileSync('changelog_template.hbs', 'utf8');
 const template = Handlebars.compile(templateSource);
 
+const commitTypes = ['fix', 'feat', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore', 'revert'];
+
+const TypeMapper = {
+    feat: 'Features',
+    fix: 'Fixes',
+    docs: 'Documentation',
+    style: 'Styles',
+    chore: 'Modify'
+}
+
+
 // Парсинг логов Git
-const parser = gitLogParser();
+const parser = gitLogParser.parse({
+    path: '',
+    format: '%H,%s,%b',
+}, {});
 
-parser.on('commit', commit => {
-    // Группировка коммитов по scope
-    const groupedCommits = groupedCommits || {};
-    const match = commit.message.match(/^(\w+)\((\w+)\):/);
-    if (match) {
-        const type = match[1];
-        const scope = match[2];
-        if (!groupedCommits[scope]) {
-            groupedCommits[scope] = {};
-        }
-        if (!groupedCommits[scope][type]) {
-            groupedCommits[scope][type] = [];
-        }
-        groupedCommits[scope][type].push({
-            subject: commit.message.replace(/^(\w+)\(\w+\): /, ''),
-            hash: commit.hash,
-        });
+function parseCommits(groupedCommits, packageName, packagePath) {
+
+    // Чтение package.json для получения версии
+    const packageJson = JSON.parse(fs.readFileSync(`${packagePath}/package.json`, 'utf8'));
+    console.log(packageJson)
+    const version = packageJson.version;
+
+    if (groupedCommits[packageName]) {
+        console.log(`Generating changelog for ${packageName}...`);
+        const changelogData = {
+            scope: packageName,
+            version: version,
+            groupedCommits: groupedCommits[packageName],
+            releaseDate: new Date().toISOString().split('T')[0]
+        };
+
+        const changelogContent = template(changelogData);
+        const changelogPath = path.join(packagePath, 'CHANGELOG.md');
+
+        fs.mkdirSync(path.dirname(changelogPath), { recursive: true });
+        fs.writeFileSync(changelogPath, changelogContent);
+    } else {
+        console.log(`No commits found for ${packageName}.`);
     }
+}
+const commits = [];
+parser.on('data', commit => {
+    commits.push(commit);
 });
-
 parser.on('end', () => {
-    // Генерация changelog для каждого scope
-    const packagesDir = 'packages';
-    fs.readdirSync(packagesDir).forEach(packageName => {
-        const packagePath = path.join(packagesDir, packageName);
-        if (fs.statSync(packagePath).isDirectory()) {
-            if (groupedCommits[packageName]) {
-                console.log(`Generating changelog for ${packageName}...`);
-                const changelogData = {
-                    scope: packageName,
-                    groupedCommits: groupedCommits[packageName],
-                };
-
-                const changelogContent = template(changelogData);
-                const changelogPath = path.join(packagePath, 'CHANGELOG.md');
-                fs.mkdirSync(path.dirname(changelogPath), { recursive: true });
-                fs.writeFileSync(changelogPath, changelogContent);
-            } else {
-                console.log(`No commits found for ${packageName}.`);
+    const groupedCommits = {};
+    commits.forEach(commit => {
+        const message = commit.message ?? commit.subject
+        const match = message.match(/^(\w+)\(([^\)]+)\):/) // (/^(\w+)\((\w+)\):/);
+        console.log({ commit, match })
+        if (match) {
+            const type = match[1];
+            const scope = match[2];
+            console.log({ match, type })
+            if (!groupedCommits[scope]) {
+                groupedCommits[scope] = {};
             }
+            if (!groupedCommits[scope][TypeMapper[type]]) {
+                groupedCommits[scope][TypeMapper[type]] = [];
+            }
+
+            if (commitTypes.includes(type)) {
+                console.log("TYPE: ", { type })
+                groupedCommits[scope][TypeMapper[type]].push({
+                    subject: message.replace(/^(\w+)\(([^\)]+)\):/, ''),
+                    body: commit.body,
+                    hash: commit.commit.short,
+                });
+            } else {
+                console.log(`Unknown commit type: ${type} in commit ${commit.hash}`);
+            }
+        }
+    });
+   
+    const packagesDir = 'packages';
+    const folders = fs.readdirSync(packagesDir)
+    folders.push('')
+
+    folders.forEach(packageName => {
+        if (!packageName) {
+            parseCommits(groupedCommits, 'root', '.')
+        } else {
+            const packagePath = path.join(packagesDir, packageName);
+            parseCommits(groupedCommits, packageName, packagePath)
         }
     });
 });
 
 parser.on('error', error => {
     console.error('Ошибка при парсинге логов:', error);
-});
-
-parser.parse({
-    path: '.',
-    format: '%H,%s,%b',
 });
