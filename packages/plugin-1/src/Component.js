bebd4c3fd55b6d9032e33da8d00d21a487600b54
Allegr@ <62800248+AllaYakymova@users.noreply.github.com>
2025-01-17 19:06:19 +0100

    chore(plugin-1): cleanup (#19)

--- packages/plugin-1/src/Component.js
+++ packages/plugin-1/src/Component.js
@@ -2,6 +2,5 @@ export function MyPlugin1(text = "Hello, Webpack!") {
     const element = document.createElement("h1");
 
     element.innerHTML = text;
-    console.log('erererer')
     return element;
 }
