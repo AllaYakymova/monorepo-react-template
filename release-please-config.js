492f34230b1988e5af9f0498c90fb27077f56b71
Alla Yakymova <allochkaja112@gmail.com>
2025-01-18 23:50:48 +0100

    chore(yamllll): smth

--- release-please-config.js
+++ release-please-config.js
@@ -7,32 +7,32 @@ module.exports = {
       changelogPath: 'packages/front-end/CHANGELOG.md',
       // Тип релиза для этого пакета
       releaseType: 'node', // или другой тип (например, 'python', 'java')
-      versioning: {
-        strategy: 'semver', // Стратегия версионирования (semantic versioning)
-        initialVersion: '0.0.1', // Начальная версия, если пакету требуется
-      },
+      // versioning: {
+      //   strategy: 'semver', // Стратегия версионирования (semantic versioning)
+      //   initialVersion: '0.0.1', // Начальная версия, если пакету требуется
+      // },
     },
 
     // Пример второго пакета package-2
     'packages/plugin-1': {
       changelogPath: 'packages/plugin-1/CHANGELOG.md',
       releaseType: 'node', // Настройка для другого пакета
-      versioning: {
-        strategy: 'semver', // Стратегия версионирования (semantic versioning)
-        initialVersion: '1.0.0', // Начальная версия, если пакету требуется
-      },
+      // versioning: {
+      //   strategy: 'semver', // Стратегия версионирования (semantic versioning)
+      //   initialVersion: '1.0.0', // Начальная версия, если пакету требуется
+      // },
     },
     // Можете добавлять другие пакеты в том же формате
   },
 
-    // Для корневого changelog
+  // Для корневого changelog
   changelogPath: 'CHANGELOG.md',
-    // Опционально: можете указать шаблон для генерации версий
-  versioning: {
-        // Настройте вашу стратегию версионирования (например, major, minor, patch)
-    strategy: 'semver', // или можно использовать другой подход
-    initialVersion: '0.1.1',
-    },
+  // Опционально: можете указать шаблон для генерации версий
+  // versioning: {
+  // Настройте вашу стратегию версионирования (например, major, minor, patch)
+  // strategy: 'semver', // или можно использовать другой подход
+  // initialVersion: '0.1.1',
+  // },
   plugins: [
     {
       type: "node-workspace"
