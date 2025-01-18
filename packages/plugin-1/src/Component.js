6f0869d4c5150b5e0a6e92bebdd9fa46a7e59c7a
Alla Yakymova <allochkaja112@gmail.com>
2025-01-18 21:16:52 +0100

    fix(Plugin): cleanup code

--- packages/plugin-1/src/Component.js
+++ packages/plugin-1/src/Component.js
@@ -1,6 +1,5 @@
 export function MyPlugin1(text = "Hello, Webpack!") {
     const element = document.createElement("h1");
-    console.log('changes')
     element.innerHTML = text;
     return element;
 }
