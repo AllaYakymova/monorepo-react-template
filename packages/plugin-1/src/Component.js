111281a1b77dc45ccd35ab35d40f4d0d63ba4d53
Alla Yakymova <allochkaja112@gmail.com>
2025-01-18 13:50:42 +0100

    fix(for plugin-1): check only plugin changes + test

--- packages/plugin-1/src/Component.js
+++ packages/plugin-1/src/Component.js
@@ -1,6 +1,6 @@
 export function MyPlugin1(text = "Hello, Webpack!") {
     const element = document.createElement("h1");
-
+    console.log('changes')
     element.innerHTML = text;
     return element;
 }
