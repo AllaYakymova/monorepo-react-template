46758b46732f7420fc35034536e3cba0d9a93dc4
Allegr@ <62800248+AllaYakymova@users.noreply.github.com>
2025-02-11 18:21:24 +0100

    Issue 444/changes in plagin (#30)
    * changes: plugin-1 and plugin-component
    
    * conflict changelog settings
    
    * Create lucky-comics-refuse.md
    
    after long consultations I decided to leave all as it is
    
    * Add meta file for changeset
    
    ---------
    
    Co-authored-by: GitHub Actions Bot <actions@github.com>

--- packages/plugin-1/src/Component.js
+++ packages/plugin-1/src/Component.js
@@ -1,5 +1,6 @@
 export function MyPlugin1(text = "Hello, Webpack!") {
     const element = document.createElement("h1");
+    console.log(element)
     element.innerHTML = text;
     return element;
 }
