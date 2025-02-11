932b243e4281a272c8fe1aed5daecdadc9859075
Alla Yakymova <allochkaja112@gmail.com>
2025-02-11 18:04:55 +0100

    changes: plugin-1 and plugin-component

--- packages/plugin-component/src/Component.ts
+++ packages/plugin-component/src/Component.ts
@@ -1,6 +1,6 @@
 export function MyPluginComponent(text = "Plugin Component Here (from monorepo)") {
     const element: HTMLElement = document.createElement("h3");
-
+    console.log({ element })
     element.innerHTML = text;
 
     return element;
