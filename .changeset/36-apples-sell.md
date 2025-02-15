d2dc03824a192c1f01c3f8ce7622353ac0dbc159
Allegr@ <62800248+AllaYakymova@users.noreply.github.com>
2025-02-15 13:14:11 +0100

    comment Component (#36)
    * comment Component
    
    * Create 36-apples-sell.md

---
+++ .changeset/36-apples-sell.md
@@ -0,0 +1,5 @@
+---
+"plugin-1": patch
+---
+
+[Chore]: [Component]: add comment
--- packages/plugin-1/src/Component.js
+++ packages/plugin-1/src/Component.js
@@ -2,5 +2,6 @@ export function MyPlugin1(text = "Hello, Webpack!") {
     const element = document.createElement("h1");
     console.log(element)
     element.innerHTML = text;
+    // there is no styles here
     return element;
 }
