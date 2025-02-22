297aea49524662d451b49aa97881c105ea1718f4
Alla Yakymova <allochkaja112@gmail.com>
2025-02-22 18:09:18 +0100

    changelog format

---
+++ .changeset/post-format.js
@@ -0,0 +1,18 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+for (const workspace of require('../package.json').workspaces) {
+    const changelogPath = path.join(__dirname, '..', workspace, 'CHANGELOG.md');
+    if (!fs.existsSync(changelogPath)) continue;
+
+    let changelog = fs.readFileSync(changelogPath, 'utf8');
+    changelog = changelog
+        .replace(/^### Major Changes/gm, '### 💥 Breaking Changes')
+        .replace(/^### Minor Changes/gm, '### ✨ Features')
+        .replace(/^### Patch Changes/gm, '### 🐛 Issues/Bugs')
+        // Remove empty sections
+        .replace(/\n### ([^\n]+)\n\n###/g, '\n###');
+    fs.writeFileSync(changelogPath, changelog);
+}
+
+// edditional here https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/scripts/release/format-changelog.js
