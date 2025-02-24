079514f4e8a182af447ccf194beb1c0d01c3306f
Alla Yakymova <allochkaja112@gmail.com>
2025-02-24 18:29:00 +0100

    add log plugin-1

--- packages/plugin-1/src/Component.js
+++ packages/plugin-1/src/Component.js
@@ -1,6 +1,8 @@
 export function MyPlugin1(text = "Hello, Webpack!") {
     const element = document.createElement("h1");
     element.innerHTML = text;
+
+    console.log('pr check')
     // there is no styles here
     return element;
 }
